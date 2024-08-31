eade1ec97ee4ba665838db1dc1591a638526de5f
gehadelsady <gehadelsady2023@gmail.com>
2024-09-01 00:15:17 +0300

    Revert "push app"
    This reverts commit d9d718c5fe8bd5520cc0b78c2f58e67b8c8a01f5.

--- lib/constants.dart
+++
@@ -1,6 +0,0 @@
-class Constants {
-  static String Token =
-      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiNjM2ZTc1OTJlNGM1MzYxYzBlYTRmMTA4YzU4NjZlYyIsIm5iZiI6MTcyNDYxNzcyOS42MDM5ODUsInN1YiI6IjY1N2Y1M2ZiNGYzM2FkMDdiMWQxY2JlYiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.j_zKmfbwThmkVZn3YFoILvrAOzCT_EGEUOn2FZlb-js";
-
-  static String BaseUrl = "api.themoviedb.org";
-}
